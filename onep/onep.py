319e50d8a1aeda32ad3d284c293c3e259b9a69d5
Antoine POPINEAU <antoine@popineau.eu>
2022-05-05 10:20:00 +0200

    Fixed search command.

--- onep/onep.py
+++ onep/onep.py
@@ -32,30 +32,30 @@ def main() -> None:
         case "search":
             search(session, args.json, args.vault, args.tags, args.term)
         case "show":
-            show(session, args.json, args.term, args.tags, args.fields, args.otp, args.select)
+            show(session, args.json, args.id, args.tags, args.fields, args.otp, args.select)
         case "share":
             share(session, args.id, args.time, args.once)
 
 
 def parse_args() -> Namespace:
     cli = ArgumentParser(prog="1p")
-    cli.add_argument("-j", "--json", action="store_true")
-    cli.add_argument("account", metavar="ACCOUNT")
+    cli.add_argument("-j", "--json", action="store_true", help="format output as JSON")
+    cli.add_argument("account", metavar="ACCOUNT", help="shorthand of the 1Password account")
 
     commands = cli.add_subparsers(dest="command", metavar="COMMAND")
 
-    commands.add_parser("signin", help="Authenticate into a 1Password account")
-    commands.add_parser("vaults", help="List available vaults")
+    commands.add_parser("signin", help="authenticate into a 1Password account")
+    commands.add_parser("vaults", help="list available vaults")
 
-    cmd_vault = commands.add_parser("vault", help="Show information about a vault")
+    cmd_vault = commands.add_parser("vault", help="show information about a vault")
     cmd_vault.add_argument("id", metavar="ID")
 
-    cmd_search = commands.add_parser("search", help="Search entries matching provided term")
+    cmd_search = commands.add_parser("search", help="search entries matching provided term")
     cmd_search.add_argument("-v", "--vault", type=str, metavar="VAULT")
     cmd_search.add_argument("-t", "--tags", type=str, metavar="TAGS")
     cmd_search.add_argument("term", metavar="TERM", nargs="*", default=[])
 
-    cmd_show = commands.add_parser("show", help="Display an entry")
+    cmd_show = commands.add_parser("show", help="display an entry")
     cmd_show_target = cmd_show.add_mutually_exclusive_group(required=True)
     cmd_show_target.add_argument("id", metavar="ID", nargs="*", default=[])
     cmd_show_target.add_argument("-t", "--tags", type=str, metavar="TAGS")
@@ -63,7 +63,7 @@ def parse_args() -> Namespace:
     cmd_show.add_argument("-o", "--otp", action="store_true")
     cmd_show.add_argument("-s", "--select", action="store_true")
 
-    cmd_share = commands.add_parser("share", help="Get a shareable link to an item")
+    cmd_share = commands.add_parser("share", help="get a shareable link to an item")
     cmd_share.add_argument("id", metavar="ID")
     cmd_share.add_argument("-t", "--time", type=str, metavar="EXPIRY")
     cmd_share.add_argument("-o", "--once", action="store_true")
